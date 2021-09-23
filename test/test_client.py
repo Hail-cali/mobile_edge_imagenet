5a77e349923766252a3010b67f14a37a369b69d9
Hail-cali <willnotproud5@gmail.com>
2021-09-23 17:25:30 +0900

    Test : modified test file to apply fl connection

--- test/test_client.py
+++ test/test_client.py
@@ -8,16 +8,17 @@ sys.path.append(WORKING_DIR_AND_PYTHON_PATHS)
 
 from server.connect import *
 from client.connect import *
-
+from opt import parse_opts
 
 CLIENT_PORT = 8080
 CLIENT_HOST = "127.0.0.1"
 
+OPT = parse_opts()
 
 async def single():
     await asyncio.wait([
                         AsyncClient(name='client_3', host=CLIENT_HOST, port=CLIENT_PORT
-                                    ).run_client(CLIENT_HOST, CLIENT_PORT),
+                                    ).run_client_model(CLIENT_HOST, CLIENT_PORT, OPT),
 
                         ])
 
