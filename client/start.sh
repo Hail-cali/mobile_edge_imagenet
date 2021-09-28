e0bb8a964e2dd459448d8e29edd0bcd957935706
Hail-cali <willnotproud5@gmail.com>
2021-09-28 14:06:46 +0900

    Fix: minor bug when run script file

--- client/start.sh
+++ client/start.sh
@@ -1,1 +1,2 @@
+cd client/
 python run_client.py --CLIENT_PORT 8080 --CLIENT_HOST '127.0.0.1' --model mobilenet
--- server/start.sh
+++ server/start.sh
@@ -1,1 +1,2 @@
+cd server/
 python run_server.py --SERVER_PORT 8080 --SERVER_HOST '127.0.0.1'
