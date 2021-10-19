0b76bb05b710d76e74bc699f8c4f8413c772aaea
Hail-cali <willnotproud5@gmail.com>
2021-10-19 16:51:28 +0900

    Add: add stream wrapper dev ============================================

--- comunicate/stream.py
+++ comunicate/stream.py
@@ -1,5 +1,26 @@
-from comunicate import request
+__all__ = (
+    'StreamReader', 'StreamWriter', 'StreamReaderProtocol',
+    'open_connection', 'start_server')
+
+# it is based on asyncio stream code
+# add & modified steam & wrapper for edge communication
+_DEFAULT_LIMIT = 2 ** 16  # 64 KiB
+import socket
+import sys
+import warnings
+import weakref
+
+if hasattr(socket, 'AF_UNIX'):
+    __all__ += ('open_unix_connection', 'start_unix_server')
+
+class BaseStream:
+
+    _source_traceback = None
+
+    def __init__(self, limit=_DEFAULT_LIMIT, loop=None):
 
 
-async def in_stream():
-    request
+        if limit <= 0:
+            raise  ValueError('Limit cannot be <=0')
+        if loop is None:
+            self._loop = ''
